959b6b5f524d030c61ec686024e9c905e26daaa1
Binada-SH <binadasasmika@gmail.com>
2025-05-24 19:23:46 +0530

    Navbar creation and displaying in the main app.jsx

--- hotel_website/src/App.jsx
+++ hotel_website/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Navbar from './Components/navbar/navbar'
 const App = () => {
   return (
     <div>
+      <Navbar/>
+
       
     </div>
   )
--- hotel_website/src/Components/navbar/navbar.jsx
+++ hotel_website/src/Components/navbar/navbar.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const navbar = () => {
   return (
-    <div>
+    <div className=' flex items-center font-[times]'>
+      <div className='w-30 mt-5 ml-15'>
+        <img src="src/assets/logo.png" alt="site_logo" />
+      </div>
+      <div className='mt-5 ml-80'>
+        <ul className='flex gap-10'>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Rooms</a></li>
+          <li><a href="">Dining</a></li>
+          <li><a href="">Contact Us</a></li>
+        </ul>
+      </div>
+      <div className='flex mt-5 ml-70 w-38 h-8 pt-1 pl-6 outline-1 outline-[#daa954] cursor-pointer'>Book your stay</div>
       
     </div>
   )
