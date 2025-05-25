0b5b0d57f9ed8e033549ff8c2364f427cf11e317
Binada-SH <binadasasmika@gmail.com>
2025-05-25 19:55:46 +0530

    Hero section image slideshow creation

--- hotel_website/src/Components/hero/hero.jsx
+++ hotel_website/src/Components/hero/hero.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const hero = () => {
+const images = [
+  'src/assets/hero_img.jpg',
+  'src/assets/hero_img_2.jpg',
+  'src/assets/hero_img_3.jpg',
+];
+
+const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prev) => (prev + 1) % images.length);
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className='fixed inset-0 z-[-10]'>
-        <div className=''>
-            <img  className='brightness-95' src="src/assets/hero_img.jpg" alt="" />
-        </div>
-      
+    <div className="fixed inset-0 z-[-10] overflow-hidden">
+      <div className="relative w-full h-full">
+        {images.map((src, index) => (
+          <img
+            key={index}
+            src={src}
+            className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-[1300ms] ease-in-out brightness-80 ${
+              index === currentIndex ? 'opacity-100' : 'opacity-0'
+            }`}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default hero
+export default Hero;
