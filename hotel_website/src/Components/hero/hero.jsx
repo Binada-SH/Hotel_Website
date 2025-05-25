19e865eb828a3f159cff2baa467a1d13852e1987
Binada-SH <binadasasmika@gmail.com>
2025-05-25 13:41:22 +0530

    Hero section ui improvments

--- hotel_website/src/Components/hero/hero.jsx
+++ hotel_website/src/Components/hero/hero.jsx
@@ -4,7 +4,7 @@ const hero = () => {
   return (
     <div className='fixed inset-0 z-[-10]'>
         <div className=''>
-            <img  className='brightness-65' src="src/assets/hero_img.jpg" alt="" />
+            <img  className='brightness-95' src="src/assets/hero_img.jpg" alt="" />
         </div>
       
     </div>
--- hotel_website/src/Components/hero_content/hero_content.jsx
+++ hotel_website/src/Components/hero_content/hero_content.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HeroContent = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center text-white">
-      <h1 className='text-[70px] font-[Cormorant_Garamond] text-center mt-20'>The Luma - Beach Resort</h1>
+    <div className="flex flex-col justify-center min-h-screen bg-cover bg-center text-white">
+      <h1 className='text-[60px] font-[Cormorant_Garamond] text-center '>The Luma , The Art of Tropical Living.</h1>
     </div>
   )
 }
