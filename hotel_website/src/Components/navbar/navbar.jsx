fb5514a5c53ea0cc660343f59b987c9213a29671
Binada-SH <binadasasmika@gmail.com>
2025-05-25 11:59:28 +0530

    Navbar ui updates

--- hotel_website/src/Components/navbar/navbar.jsx
+++ hotel_website/src/Components/navbar/navbar.jsx
@@ -4,7 +4,7 @@ const navbar = () => {
   return (
     <div className=' flex items-center font-[times] z-10 text-[#ffffff] text-[18px] fixed'>
       <div className='w-30 mt-5 ml-15'>
-        <img src="src/assets/logo.png" alt="site_logo" />
+        <img className='cursor-pointer' src="src/assets/logo.png" alt="site_logo"/>
       </div>
       <div className='mt-5 ml-80'>
         <ul className='flex gap-10'>
@@ -15,7 +15,7 @@ const navbar = () => {
           <li><a href="" className='hover:text-[#daa954] transition-colors'>Contact Us</a></li>
         </ul>
       </div>
-      <div className='flex mt-5 ml-70 w-38 h-10 pt-1.5 pl-5 outline-1 outline-[#daa954] cursor-pointer'>Book your stay</div>
+      <div className='flex mt-5 ml-70 w-38 h-10 pt-1.5 pl-5 outline-1 outline-[#daa954] cursor-pointer hover:bg-[#daa954] transition-colors'>Book your stay</div>
       
     </div>
   )
