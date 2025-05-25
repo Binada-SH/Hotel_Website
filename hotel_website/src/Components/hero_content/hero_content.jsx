b970efdb42a67df0e68d64de697457296cf0ca5e
Binada-SH <binadasasmika@gmail.com>
2025-05-25 19:15:45 +0530

    Hero section slogan adding

--- hotel_website/src/Components/hero_content/hero_content.jsx
+++ hotel_website/src/Components/hero_content/hero_content.jsx
@@ -4,7 +4,9 @@ const HeroContent = () => {
   return (
     <div className="flex flex-col justify-center min-h-screen bg-cover bg-center text-white">
       <h1 className='text-[60px] font-[Cormorant_Garamond] text-center '>The Luma , The Art of Tropical Living.</h1>
+      <h4 className='text-[18px] font-[now] text-center'>Luxury meets Tropical</h4>
     </div>
+  
   )
 }
 
