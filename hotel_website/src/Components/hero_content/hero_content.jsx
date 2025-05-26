226e89367c9a08e78363c729ca0a0cdb2a8476ec
Binada-SH <binadasasmika@gmail.com>
2025-05-26 17:53:12 +0530

    Hero content cta hover effect adding

--- hotel_website/src/Components/hero_content/hero_content.jsx
+++ hotel_website/src/Components/hero_content/hero_content.jsx
@@ -6,7 +6,7 @@ const HeroContent = () => {
       <h2 className='text-[22px] text-center mt-30 font-[Cormorant_Garamond] text-[#daa954]'>The Luma Beach Resort</h2>
       <h1 className='text-[21px] text-center mt-4 font-[now] text-[#000000]'>A Hidden Coastal Sanctuary Where Minimalism Meets Nature</h1>
       <p className='text-[15px] text-center w-150 m-auto mt-11 font-[jura] uppercase leading-8'>A serene escape on Sri Lanka’s southern coast, The Luma blends minimalist design with the natural beauty of the ocean. With panoramic sea views, private villas, and curated local experiences, it’s a peaceful retreat where simplicity meets coastal charm.</p>
-      <button className='mt-15 bg-[#eab559] w-35 h-10 m-auto font-[Cormorant_Garamond] text-[17px] text-[#ffffff]'>Explore More</button>
+      <button className='mt-15 bg-[#eab559] w-35 h-10 m-auto font-[Cormorant_Garamond] text-[17px] text-[#ffffff] hover:text-[#000000] cursor-pointer'>Explore More</button>
     </div>
   
   )
--- hotel_website/src/Components/rooms/rooms.jsx
+++ hotel_website/src/Components/rooms/rooms.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const rooms = () => {
   return (
     <div>
+      <h1>Our Rooms</h1>
       
     </div>
   )
