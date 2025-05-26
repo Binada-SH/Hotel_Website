48b8bd1a896b54036e256eeb07e177bac9ac9458
Binada-SH <binadasasmika@gmail.com>
2025-05-26 19:33:58 +0530

    Rooms details logic change and displaying

--- hotel_website/src/Components/rooms/rooms.jsx
+++ hotel_website/src/Components/rooms/rooms.jsx
@@ -1,28 +1,40 @@
-import { img } from 'framer-motion/client';
 import React from 'react'
 
-const images = [
-  'src/assets/suite.jpg',
-  'src/assets/deluxe.jpg',
-  'src/assets/supirior.jpg',
+const room= [
+  {
+    src: 'src/assets/suite.jpg', 
+    title: 'Suite Rooms', 
+    description: 'Spacious and serene, our Suites offer refined luxury with sweeping views, curated interiors, and elevated comfort for an indulgent escape.'
+  },
+  {
+    src: 'src/assets/supirior.jpg', 
+    title: 'Supirior Rooms', 
+    description: 'A blend of natural textures and modern simplicity, the Superior Rooms provide a peaceful space to unwind, with views of lush greenery.'
+  },
+  {
+    src: 'src/assets/deluxe.jpg', 
+    title: 'Deluxe Rooms', 
+    description: 'Cozy yet sophisticated, our Deluxe Rooms feature minimalist design, ambient light, and all the essentials for a relaxed coastal stay.'
+  }
 ];
 
-const rooms = () => {
+const Rooms = () => {
   return (
     <div className='bg-[#F8F0E5]'>
       <h1 className='text-[22px] text-center mt-30 pt-10 font-[Cormorant_Garamond] text-[#daa954]'>Living Spaces</h1>
       <h3 className='text-[21px] text-center mt-4 font-[now] text-[#000000]'>Naturally Inspired Spaces, Defined by Minimalist Comfort</h3>
       <p className='text-[15px] text-center w-190 m-auto mt-11 font-[jura] uppercase leading-8 text-[#000000]'>Each room reflects the calm, open spirit of the coast, with clean lines, earthy tones, and natural textures. Whether it’s a garden-view room or an ocean-facing villa, you’ll enjoy warm light, soft linens, and a peaceful space designed for rest and renewal.</p>
-      <div className='grid grid-cols-3 mt-10 px-4'>
-        {images.map((src,index) => (
-          <img
-            key = {index} 
-            src = {src} 
-            className='w-full h-auto object-cover'   />
-        ))}
-      </div>      
+      <div className='grid grid-cols-3 mt-40 gap-6 px-20'>
+        {room.map((room,index) => (
+          <div key = {index}>
+            <img src = {room.src}/>
+            <h1 className=' mt-4 text-[24px] font-[now] text-center text-[#000000]'>{room.title}</h1>
+            <p className='mt-6 w-85 text-center text-[18px] m-auto font-[Cormorant_Garamond]'>{room.description}</p>
+          </div>
+        ))}      
+      </div>
     </div>
-  )
-}
+    );
+};
 
-export default rooms
+export default Rooms
