d270677273e90808a5a9997991e7799cd3d2d4ed
Binada-SH <binadasasmika@gmail.com>
2025-05-26 10:05:08 +0530

    New component adding in the main file

--- hotel_website/src/App.jsx
+++ hotel_website/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from './Components/navbar/navbar'
 import Hero from './Components/hero/hero'
 import HeroContent from './Components/hero_content/hero_content'
+import Rooms from './Components/rooms/rooms'
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
       <HeroContent/>
-
+      <Rooms/>
     </div>
   )
 }
