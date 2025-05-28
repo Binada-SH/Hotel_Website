97db1aa8930ccaa219a151eec6d810f3b63c00c9
Binada-SH <binadasasmika@gmail.com>
2025-05-28 23:36:11 +0530

    Hero section responsive design

--- hotel_website/src/Components/hero_content/hero_content.jsx
+++ hotel_website/src/Components/hero_content/hero_content.jsx
@@ -5,7 +5,7 @@ const HeroContent = () => {
     <div className="flex flex-col justify-center">
       <h2 className='text-[22px] text-center mt-30 font-[Cormorant_Garamond] text-[#daa954]'>The Luma Beach Resort</h2>
       <h1 className='text-[21px] text-center mt-4 font-[now] text-[#000000]'>A Hidden Coastal Sanctuary Where Minimalism Meets Nature</h1>
-      <p className='text-[15px] text-center w-150 m-auto mt-11 font-[jura] uppercase leading-8'>A serene escape on Sri Lanka’s southern coast, The Luma blends minimalist design with the natural beauty of the ocean. With panoramic sea views, private villas, and curated local experiences, it’s a peaceful retreat where simplicity meets coastal charm.</p>
+      <p className='w-96 sm:w-150 text-[15px] text-center m-auto mt-11 font-[jura] uppercase leading-8'>A serene escape on Sri Lanka’s southern coast, The Luma blends minimalist design with the natural beauty of the ocean. With panoramic sea views, private villas, and curated local experiences, it’s a peaceful retreat where simplicity meets coastal charm.</p>
       <button className='mt-15 bg-[#daa954] w-35 h-10 m-auto font-[Cormorant_Garamond] text-[17px] text-[#ffffff] hover:text-[#000000] cursor-pointer transition-colors'>Explore More</button>
     </div>
   
