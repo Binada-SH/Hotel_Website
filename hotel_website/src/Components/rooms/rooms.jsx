ec12f6825c86bca1e0d9e32be4cb56be8f6876af
Binada-SH <binadasasmika@gmail.com>
2025-05-28 16:20:04 +0530

    Rooms section minor ui updates

--- hotel_website/src/Components/rooms/rooms.jsx
+++ hotel_website/src/Components/rooms/rooms.jsx
@@ -42,9 +42,9 @@ const Rooms = () => {
               <div className="w-[3px] mt-16 h-6 bg-[#daa954]"></div>
               <p className='mt-16 w-85'>{room.view} View</p>
             </div> 
-            <div className='mt-20 flex gap-5 px-15 text-center justify-center font-[Cormorant_Garamond] text-[19px] pb-30'>
-              <h3 className='w-40 h-10 border-1 border-[#daa954] pt-1.5 hover:text-[#daa954] transition-colors'><a href="">More Info</a></h3>        
-              <h3 className='bg-[#daa954] w-40 pt-1.5 text-[#ffffff] hover:text-[#000000] transition-colors'><a href="">Book Now</a></h3> 
+            <div className='mt-20 flex gap-5 px-15 text-center justify-center font-[Cormorant_Garamond] text-[17px] pb-30'>
+              <h3 className='w-40 h-8 border-1 border-[#daa954] pt-0.5 hover:text-[#daa954] transition-colors'><a href="">More Info</a></h3>        
+              <h3 className='bg-[#daa954] w-40 pt-0.5 text-[#ffffff] hover:text-[#000000] transition-colors'><a href="">Book Now</a></h3> 
             </div>    
           </div>
         ))}      
