4f16d705e68670c606494bacd56f8e5aa99339ca
Binada-SH <binadasasmika@gmail.com>
2025-05-28 23:47:57 +0530

    Room details responsive design

--- hotel_website/src/Components/rooms/rooms.jsx
+++ hotel_website/src/Components/rooms/rooms.jsx
@@ -29,14 +29,14 @@ const Rooms = () => {
     <div className='bg-[#F8F0E5]'>
       <h1 className='text-[22px] text-center mt-30 pt-30 font-[Cormorant_Garamond] text-[#daa954]'>Living Spaces</h1>
       <h3 className='text-[21px] text-center mt-4 font-[now] text-[#000000]'>Naturally Inspired Spaces, Defined by Minimalist Comfort</h3>
-      <p className='text-[15px] text-center w-190 m-auto mt-11 font-[jura] uppercase leading-8 text-[#000000]'>Each room reflects the calm, open spirit of the coast, with clean lines, earthy tones, and natural textures. Whether it’s a garden-view room or an ocean-facing villa, you’ll enjoy warm light, soft linens, and a peaceful space designed for rest and renewal.</p>
-      <div className='grid grid-cols-3 mt-40 gap-6 px-20'>
+      <p className='w-96 sm:w-190 text-[15px] text-center m-auto mt-11 font-[jura] uppercase leading-8 text-[#000000]'>Each room reflects the calm, open spirit of the coast, with clean lines, earthy tones, and natural textures. Whether it’s a garden-view room or an ocean-facing villa, you’ll enjoy warm light, soft linens, and a peaceful space designed for rest and renewal.</p>
+      <div className='px-8 grid-cols-3 mt-40 gap-6 lg:grid lg:px-20 '>
         {room.map((room,index) => (
           <div key = {index}>
             <img  className='cursor-pointer hover:scale-101 transition-all ' src = {room.src} alt={room.title}/>
             <h1 className=' mt-4 text-[24px] font-[now] text-center text-[#000000]'><a href="" className='cursor-pointer'>{room.title}</a></h1>
             <hr className='text-[#daa954] w-30 m-auto mt-3'/>
-            <p className='mt-6 w-85 text-center text-[18px] m-auto font-[jura]'>{room.description}</p>  
+            <p className='w-85 mt-6  text-center text-[18px] m-auto font-[jura]'>{room.description}</p>  
             <div className='flex items-center px-10 justify-center text-[18px] font-[now] text-center'>
               <p className='mt-16 w-85'>{room.number} Rooms</p>
               <div className="w-[3px] mt-16 h-6 bg-[#daa954]"></div>
